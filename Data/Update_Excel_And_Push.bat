37f25725cb58d9cb27757930a68878aa839fe383
Tiko <marouane.ghailan.tiko@gmail.com>
2023-04-28 13:50:40 +0200

    Update scipts

--- Data/Update_Excel_And_Push.bat
+++ Data/Update_Excel_And_Push.bat
@@ -8,21 +8,7 @@ set VISION_GENERAL="C:\Users\Tiko\Desktop\Tiko\investissement\Gestion de crypto\
 rem Exécuter getData.py
 echo Exécution de getData.py...
 start "getData.py" %DATA_PY%
-TIMEOUT 4
+TIMEOUT 20
 
-rem Appeler la fonction d'ouverture des fichiers
-call :ouvrirFichier %HISTORIQUE_ACHATS% "Historique d achats.xlsx"
-call :ouvrirFichier %VISION_GENERAL% "Vision General.xlsx"
-start push_Changes.sh
-
-goto :eof
-
-rem Fonction pour ouvrir un fichier s'il existe
-:ouvrirFichier
-if exist %1 (
-    echo Ouverture de %2...
-    start %2 %1
-) else (
-    echo Fichier %2 introuvable.
-)
-goto :eof
+rem Pousser les modif sur git
+"C:\Program Files\Git\git-bash.exe" "C:\Users\Tiko\Desktop\Tiko\investissement\Gestion de crypto\Data\push_Changes.sh"
---
+++ Data/push_Changes.sh
@@ -0,0 +1,5 @@
+cd "C:\Users\Tiko\Desktop\Tiko\investissement\Gestion de crypto"
+git add "Data\Historique d'achats.xlsx"
+git add "Data\Vision General.xlsx"
+git commit -m "Update Excel files"
+git push -u origin main
