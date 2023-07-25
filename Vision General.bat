61f775b5965104a63372ac2455ff77cb5f746f07
Tiko <marouane.ghailan.tiko@gmail.com>
2023-07-25 23:25:27 +0200

    Update Excel files

--- Vision General.bat
+++ Vision General.bat
@@ -1,9 +1,9 @@
 @echo off
 
 rem Définir les variables pour les chemins de fichiers
-set DATA_PY="C:\Users\Tiko\Desktop\Tiko\investissement\Gestion de crypto\Data\getData.py"
-set HISTORIQUE_ACHATS="C:\Users\Tiko\Desktop\Tiko\investissement\Gestion de crypto\Data\Historique d'achats.xlsx"
-set VISION_GENERAL="C:\Users\Tiko\Desktop\Tiko\investissement\Gestion de crypto\Data\Vision General.xlsx"
+set DATA_PY="Data\getData.py"
+set HISTORIQUE_ACHATS="Data\Historique d'achats.xlsx"
+set VISION_GENERAL="Data\Vision General.xlsx"
 
 rem Exécuter getData.py
 echo Exécution de getData.py...
@@ -13,7 +13,7 @@ TIMEOUT 4
 rem Appeler la fonction d'ouverture des fichiers
 call :ouvrirFichier %HISTORIQUE_ACHATS% "Historique d achats.xlsx"
 call :ouvrirFichier %VISION_GENERAL% "Vision General.xlsx"
-"C:\Program Files\Git\git-bash.exe" "C:\Users\Tiko\Desktop\Tiko\investissement\Gestion de crypto\Data\push_Changes.sh"
+"C:\Program Files\Git\git-bash.exe" "Data\push_Changes.sh"
 
 goto :eof
 
