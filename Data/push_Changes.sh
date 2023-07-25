e78c2fecbd22e2e75cdfab05d4931ce01ea45724
Tiko <marouane.ghailan.tiko@gmail.com>
2023-07-25 23:26:27 +0200

    Update Excel files

--- Data/push_Changes.sh
+++ Data/push_Changes.sh
@@ -1,5 +1,8 @@
 cd "C:\Users\Tiko\Desktop\Tiko\investissement\Gestion de crypto"
 git add "Data\Historique d'achats.xlsx"
 git add "Data\Vision General.xlsx"
+git add "Data\getData.py"
+git add "Data\push_Changes.sh"
+git add "Vision General.bat"
 git commit -m "Update Excel files"
 git push -u origin main
