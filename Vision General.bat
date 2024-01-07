8cb3647441b869c382cd91b8dc5bcf3020b03f76
Tiko <marouane.ghailan.tiko@gmail.com>
2024-01-07 01:14:27 +0100

    Update Excel files

--- Vision General.bat
+++ Vision General.bat
@@ -1,10 +1,7 @@
 @echo off
 
-rem Définir les variables pour les chemins de fichiers
-set DATA_PY="Data\getData.py"
-
 rem Exécuter getData.py
 echo Exécution de getData.py...
-start "getData.py" %DATA_PY%
+start "getData.py" Data\getData.py
 
 
