6bbb29373b7b941e1a3dac359c48f60917d2dc6b
Tiko <marouane.ghailan.tiko@gmail.com>
2024-02-07 22:49:37 +0100

    Update automatic push to push only excel files

--- Data/push_Changes.sh
+++ Data/push_Changes.sh
@@ -1,11 +1,5 @@
 cd "C:\Users\Tiko\Desktop\Tiko\investissement\Gestion de crypto"
 git add "Data\Historique d'achats.xlsx"
 git add "Data\Vision General.xlsx"
-git add "Data\getData.py"
-git add "Data\get_cumulative_total_rewards.py"
-git add "Data\get_auto_invest_amount.py"
-git add "Data\push_Changes.sh"
-git add "Vision General.bat"
-git add "README.md"
-git commit -m "Automatic push"
+git commit -m "Automatic push - Update excel files"
 git push
